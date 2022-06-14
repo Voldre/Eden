0ac03e0aaad4e41785fd74b773544784c16341df
Dubuc Valentin <v.dre@laposte.net>
2022-06-15 00:55:43 +0200

    Add illustrations and dummy company
    Illustrations and fan-art of Eden Eternal
    Dummy company in order to give an example of a company website : Feravena.

---
+++ offers.php
@@ -0,0 +1,38 @@
+<?php
+include("header.php");
+include("menu2.html");
+?>
+<body>
+<h2>Les offres de Feravena :</h2>
+<div style="border:2px black solid;padding:1.5%;">
+<h2>Développement de ressources :</h2>
+<p>Besoin d'ajouter de nouveaux designs 2D ou 3D à votre jeu ? Envie de nouvelles pistes sonores ? Ou encore besoin d'améliorer des ressources existantes ? Feravena est là pour répondre à ces besoins !</p>
+<p>Grâce à notre expérience acquise au cours de <a href="success.php">projets avec nos clients</a>, nous avons toutes les clés en main pour relever vos défis artistiques ! Nous serons ravi de vous compter parmi nos clients et d'ajouter votre nom dans notre liste de partenaires.</p>
+<p>Nous répondrons à toutes vos demandes en terme de :</p>
+<ul style="line-height:170%;">
+    <li>Conception de ressources 3D</li>
+    <li>Animation de ressources 3D</li>
+    <li>Création d'images et illustrations</li>
+    <li>Composition de pistes sonores</li>
+</ul>
+<div class="flexContainer">
+<img src="images/screen_1.jpg" style="width:45%;text-align:center;margin-right:2px;"/>
+<img src="images/screen_2.jpg" style="width:45%;text-align:center;"/>
+</div>
+</div>
+<div style="border:2px black solid;padding:1.5%;">
+<h2>Achat de ressources :</h2>
+<p>Vous avez exploré notre catalogue de <a href="database.php">ressources</a> et de <a href="3D.php">conceptions 3D</a>, et vous avez flashé sur certaines d'entre-elles ? Cela tombe bien, vous pouvez les obtenirs !</p>
+<p>Nos ressources sont accessible, avec une possibilité d'obtenir des versions alternatives de ces ressources (dimensions, couleurs, textures, ...).</p>
+<div class="flexContainer">
+<img src="images/screen_3.png" style="border:1px wheat solid; width:77%;text-align:center;"/>
+</div>
+</div>
+<div style="border:2px black solid;padding:1.5%;">
+<h2>Accès à nos outils de conceptions :</h2>
+<p>Vous appréciez nos conceptions et vous pensez que les logiciels pour les créés sont trop onéreux ? Eh bien non, ils sont moins chers que les équivalents sur le marché ! Ainsi Feravena peut vous mettre à disposition ses outils afin d'aider vous et votre équipe à développer vos propres ressources !</p>
+VIDEO
+<p>Notre suite d'outils comprend un concepteur 3D et 2D, un outil d'animation et un outil d'édition sonore (uniquement d'édition).</p>
+</div>
+</body>
+</html>
--- univers.php
+++ univers.php
@@ -1,6 +1,6 @@
 <?php
 include("header.php");
-include("menu.html");
+include("menu2.html");
 ?>
 <body>
 <div class="dbMenu">
