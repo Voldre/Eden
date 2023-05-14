0b5fd97f2f629bcc25bfcad723b283eacfd8e628
Dubuc Valentin <v.dre@laposte.net>
2023-05-14 23:42:25 +0200

    New pictures, classes desc, new perso, display eqpt list
    Main fix : update cookie to get only new data (example : only updated character).
    Because cookie has max size.

--- JDRsaveFile.php
+++ JDRsaveFile.php
@@ -2,19 +2,15 @@
 
 $name = $_POST['name'];
 
-if($name == "persos" && isset($_COOKIE[$name.'JSON'])) {
-    file_put_contents('JDR'.$name.'.json', $_COOKIE[$name.'JSON']);
+if(isset($_COOKIE[$name.'JSON'])) {
+
     print_r($_COOKIE[$name.'JSON']);
-}
-
-
-if(($name == "skills" || $name == "eqpt" || $name == "master" || $name == "enemy") && isset($_COOKIE[$name.'JSON'])) {
-
+    echo "<br/><br/>";
+    print_r(json_decode($_COOKIE[$name.'JSON']));
     $file = file_get_contents('JDR'.$name.'.json', true);
     $file = json_decode($file);
-    print_r($file);
+    // print_r($file);
 
-    
     foreach(json_decode($_COOKIE[$name.'JSON']) as $key => $value) {
         $file->$key = $value;
         print_r($key);
