a4e8fa3376a43760951f8630417bae08505a00d1
Dubuc Valentin <v.dre@laposte.net>
2023-10-04 20:37:32 +0200

    Ajout ChatGPT + correctifs + contenus

---
+++ JDRlogin_chatgpt.php
@@ -0,0 +1,12 @@
+<?php $specialPath = getenv('chatgpt_api'); 
+
+if(!isset($specialPath)){
+    return false
+}else{
+    return $specialPath
+}
+
+?>
+
+<meta http-equiv="refresh" content="5; URL=./jdr.html">
+
