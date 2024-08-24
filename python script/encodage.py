cfb58db8aa9f2e792ef851d131e5d1d902bb313e
Dubuc Valentin <v.dre@laposte.net>
2024-08-25 00:12:50 +0200

    Many few updates
    New monsters icons, PP, backup.
    Update rules with malus duration, % health bonus.
    Add some data and info.
    Many updates for enemies stats/damages.

---
+++ python script/encodage.py
@@ -0,0 +1,97 @@
+import os
+import chardet
+import fnmatch
+import unicodedata
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+
+def add_space_after_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    
+    # Construire une nouvelle chaîne en ajoutant un espace après chaque caractère accentué
+    output_str = ''
+    for c in nfkd_form:
+        if unicodedata.combining(c):  # Si c'est un accent
+            output_str += c + '_'  # Ajouter l'accent et un _
+        else:
+            output_str += c  # Ajouter le caractère sans modification
+    
+    # Normaliser la chaîne pour recombiner les accents avec leurs caractères de base
+    return unicodedata.normalize('NFC', output_str)
+
+def replace_special_characters(input_str):
+    replacements = {
+        '«': '"',
+        '»': '"',
+        '…': '...',
+        '‘': ''',
+        '’': ''',
+        '“': '"',
+        '”': '"',
+        '–': '-',
+        '—': '--'
+    }
+    for char, replacement in replacements.items():
+        input_str = input_str.replace(char, replacement)
+
+    return input_str
+
+
+def convertfile_encoding(filename, file_path, output_path, from_encoding='Windows-12', to_encoding='ANSI'):# to_encoding='utf-8'):
+    # Lire le contenu du fichier d'entrée
+
+    if False :
+
+        with open(file_path, 'rb') as file:
+            raw_data = file.read()
+
+        # Détecter l'encodage
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+        confidence = result['confidence']
+
+        print(f"Fichier : {filename} - Encodage : {encoding}, confiance {round(confidence,2)*100}%\n")
+
+        # Si l'encodage détecté match celui attendu, on procède à la conversion
+        # if from_encoding in encoding :
+            
+        # Lire le fichier avec l'encodage d'origine et le convertir en UTF-8
+        with open(file_path, 'r', encoding=encoding, errors='ignore') as file:
+            content = file.read()
+
+    # Lire le fichier
+    with open(file_path, 'r', errors='ignore') as file:
+        content = file.read()
+
+    # Remplacer les caractères spécifiques
+    content_no_accents = add_space_after_accents(content)  # content # remove_accents(content)
+    content_cleaned = replace_special_characters(content_no_accents)
+
+    with open(output_path, 'w',errors='ignore') as file:#, encoding=to_encoding, errors='ignore') as file:
+        file.write(content_cleaned)
+        # print(f"Fichier converti en UTF-8 et sauvegardé sous {output_path}")
+        print(f"Fichier sauvegardé {output_path}")
+
+    # else:
+    #     print(f"L'encodage détecté n'est pas {from_encoding}. Aucune conversion effectuée pour {filename}.")
+
+
+def convert_directory_files(directory, file_pattern='t_*'):         # \db_utf8
+    returned_path = r'C:\Users\ec\Downloads\Downloader\Extract2\data\db_ansi\\'
+    # Parcourir tous les fichiers dans le répertoire spécifié
+    for root, _, files in os.walk(directory):
+        for filename in fnmatch.filter(files, file_pattern):
+            file_path = os.path.join(root, filename)
+            output_path = f"{returned_path}{filename}"
+
+            # Convertir l'encodage du fichier
+            convertfile_encoding(filename,file_path, output_path)
+
+# Chemin vers le répertoire contenant les fichiers
+directory_path = r'C:\Users\ec\Downloads\Downloader\Extract2\data\db'
+
+# Convertir tous les fichiers commençant par "t" dans le répertoire spécifié
+convert_directory_files(directory_path)
---
+++ python script/itemmall.py
@@ -0,0 +1,60 @@
+import psycopg2
+from psycopg2 import sql
+from tqdm import tqdm
+
+#  python3 '.\python script\itemmall.py'
+
+# Paramètres de connexion à la base de données
+conn_params = {
+    'dbname': 'fnaccount', # 'fndb1', # or fnaccount
+    'user': 'postgres',
+    'password': 'eden',
+    'host': '192.168.1.33',  # ou l'adresse IP du serveur PostgreSQL
+    'port': '5432',  # port par défaut de PostgreSQL
+}
+
+try:
+    # Connexion à la base de données
+    conn = psycopg2.connect(**conn_params)
+    cursor = conn.cursor()
+
+    print('connected')
+    
+    money_ezur_point=3
+    group_amelio=5
+
+    cursor.execute('SELECT current_database();')
+
+    print(cursor.fetchall())
+    
+    # Reset itemmall for this group
+    cursor.execute("DELETE FROM itemmall WHERE item_group = %s AND money_unit = %s;",(group_amelio,money_ezur_point,))
+    # Exemple d'insertion de données dans une table
+    insert_query = """
+    INSERT INTO itemmall (item_id,item_group,item_index, item_num,money_unit,sell, point, special_price,item_color,sell_date, not_sell_date,num_limit,recognized_percentage, level_limit)
+    VALUES (%s, %s, %s,  %s, %s,%s, 1, 0,0,0, 0,0,0, 0);
+    """
+
+    first_id = 36792
+    last_id = 36951
+
+    for index,id in tqdm(enumerate(range(first_id,last_id))):
+        data = (id,group_amelio,index, 1,money_ezur_point,1)
+        cursor.execute(insert_query, data)
+
+    # Validation des modifications
+    conn.commit()
+
+    print("Les données ont été insérées avec succès!")
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print(f"Erreur lors de l'insertion de données : {error}")
+    # En cas d'erreur, annuler les modifications
+    conn.rollback()
+
+finally:
+    # Fermer la connexion
+    if conn:
+        cursor.close()
+        conn.close()
+        print("La connexion à la base de données a été fermée.")
